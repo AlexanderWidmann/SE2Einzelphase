3a16df3981abb8c10f9c2be46d9e237061963a99
Alexander Widmann <alexanderwi@edu.aau.at>
2017-03-13 14:28:17 +0100

    Added: Code ->Inintialisierung an die richtige Stelle verschoben UI: Input erlaubt auch negative und decimal-Zahlen

--- app/src/main/java/alexanderwidmann/se2einzelphase/Divisionsrechner.java
+++ app/src/main/java/alexanderwidmann/se2einzelphase/Divisionsrechner.java
@@ -5,17 +5,24 @@ import android.os.Bundle;
 import android.view.*;
 import android.widget.*;
 public class Divisionsrechner extends AppCompatActivity {
-    //Benötigte Felder laden (2 Input felder und das Output Feld)
-    EditText divisor= (EditText) findViewById(R.id.eTDivisor);
-    EditText dividend= (EditText) findViewById(R.id.eTDividend);
-    TextView output=(TextView)findViewById(R.id.tVShowOutput);;
+    EditText divisor, dividend;
+    TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_divisionsrechner);
+        //Benötigte Felder laden (2 Input felder und das Output Feld)
+        try {
+            divisor = (EditText) findViewById(R.id.eTDivisor);
+            dividend = (EditText) findViewById(R.id.eTDividend);
+            output = (TextView) findViewById(R.id.tVShowOutput);
+        }catch(Exception e){
+            System.out.println(e.getMessage());//debugging Exceptionhandling
+        }
     }
 
     protected void divide(View view){
+
        try{
            //a/b = c
            double a = Double.parseDouble(dividend.getText().toString());//Umformungen nur für Java nötig(da input typ Zahlen sind)
@@ -23,7 +30,7 @@ public class Divisionsrechner extends AppCompatActivity {
            double c = a/b;
            output.setText(c+"");
        }catch(Exception e){
-           //standard exception handling -> Exception message wird im output feld angezeigt falls eine solche auftreten sollte
+           //standard exception handling -> Exception message wird im output-feld angezeigt falls eine solche auftreten sollte
            output.setText(e.getMessage());
        }
     }
