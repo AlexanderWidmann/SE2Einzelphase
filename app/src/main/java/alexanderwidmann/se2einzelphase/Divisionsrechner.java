2dc0302d9f4d2ac7917193e8a15c46930355fcdd
Alexander Widmann <Alexander Widmann>
2017-03-13 11:47:32 +0100

    Initial commit

---
+++ app/src/main/java/alexanderwidmann/se2einzelphase/Divisionsrechner.java
@@ -0,0 +1,13 @@
+package alexanderwidmann.se2einzelphase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Divisionsrechner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_divisionsrechner);
+    }
+}
